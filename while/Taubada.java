20f92efa7f8cb835bee029cc980b5af30c15d3bb
ZakWegues <zakduwe@gmail.com>
2025-11-23 12:37:11 -0300

    Create Taubada.java

---
+++ while/Taubada.java
@@ -0,0 +1,23 @@
+package While;
+
+import java.util.Scanner;
+
+public class Tabuada {
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		int multiplicando, multiplicador = 1, resultado;
+		
+		System.out.println("digite um número");
+		multiplicando = entrada.nextInt();
+		
+		while (multiplicador<=10) {
+			
+			resultado = multiplicador*multiplicando;
+			System.out.println(multiplicando+"x"+multiplicador+"= "+resultado);
+			multiplicador++;
+		}
+		entrada.close();
+	}
+}
