c7b3f195aa418e0c446bd4fd9ee289130f767b7b
ZakWegues <zakduwe@gmail.com>
2025-11-22 21:10:25 -0300

    Add BaseAltura class to calculate triangle area

---
+++ EPS/BxA.java
@@ -0,0 +1,24 @@
+package EPS;
+import java.util.Scanner;
+
+public class BaseAltura {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		int altura, base;
+		double areaCalculada;
+		
+		System.out.println("Digite a base");
+		base = entrada.nextInt();
+		System.out.println("Digite a altura");
+		altura = entrada.nextInt();
+
+		areaCalculada = (base * altura) / 2.0;
+		
+		System.out.println("area: " + areaCalculada);
+
+	entrada.close();
+	}
+
+}
