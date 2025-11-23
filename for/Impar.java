61e6197b923a98e356fa18fe0c10566fc9de8b64
ZakWegues <zakduwe@gmail.com>
2025-11-23 12:31:14 -0300

    Create Impar.java

---
+++ for/Impar.java
@@ -0,0 +1,16 @@
+package For;
+
+public class Impar {
+
+	public static void main(String[] args) {
+		
+		int numero=1;
+		
+		for (numero=1; numero<1000; numero=numero+2) {
+			System.out.println(numero);
+		}
+
+
+    
+	}
+}
