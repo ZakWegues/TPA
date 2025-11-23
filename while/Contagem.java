762e55587fff04a8d59b8efadecdff7f8d4b0749
ZakWegues <zakduwe@gmail.com>
2025-11-23 12:34:40 -0300

    Add Contagem.java to count from 1 to 100

---
+++ while/Contagem.java
@@ -0,0 +1,14 @@
+package While;
+
+public class Contagem {
+	public static void main(String[] args) {
+
+		int contador = 1;
+		
+		while (contador<=100) {
+			System.out.println(contador);
+			contador++;
+		}
+
+	}
+}
