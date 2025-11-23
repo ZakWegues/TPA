86f9fabdcc8e9e93d5e635216faf2e952bfe4c24
ZakWegues <zakduwe@gmail.com>
2025-11-23 12:58:23 -0300

    Create Atividade3.java

---
+++ vetores/Atividade3.java
@@ -0,0 +1,41 @@
+package vetor;
+import java.util.Scanner;
+
+public class At3Primo {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner (System.in);
+		
+		int vetorNumeros[], indice=0;
+		
+		final int TAMANHO=10;
+		
+		vetorNumeros = new int [TAMANHO];
+		
+		
+		for (indice=0; indice<TAMANHO; indice++) {
+			System.out.println("Digite o "+(indice+1)+" número");
+			vetorNumeros[indice] = entrada.nextInt();
+		}
+		
+		
+		for (indice=0; indice<TAMANHO; indice++) {
+			
+			if (vetorNumeros[indice]==2 || vetorNumeros[indice]==3 ||  vetorNumeros[indice]==5 || vetorNumeros[indice]==7) {
+				System.out.println(vetorNumeros[indice]+" É primo");
+				} 
+				
+				else { if (vetorNumeros[indice]%2==0 || vetorNumeros[indice]%3==0 || vetorNumeros[indice]%5==0 || vetorNumeros[indice]%7==0 || vetorNumeros[indice]%9==0) {
+					System.out.println(vetorNumeros[indice]+" Não é primo");
+					} else {
+						System.out.println(vetorNumeros[indice]+" É primo");
+					}
+				}	
+		}
+		entrada.close();
+
+
+    
+	}
+
+}
