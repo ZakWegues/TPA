186154e1f70712b1e23b709cd1bff12838bbd659
ZakWegues <zakduwe@gmail.com>
2025-11-22 21:52:14 -0300

    Create Soma.java

---
+++ doWhile/Soma.java
@@ -0,0 +1,16 @@
+package DoWhile;
+
+public class Soma {
+
+	public static void main(String[] args) {
+		
+		int contador = 0;
+		
+		do {
+			contador = contador + 2;
+			System.out.println(contador);
+		} while (contador < 500);
+
+    
+	}
+}
