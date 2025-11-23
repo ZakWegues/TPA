b29b6a2091b5ee6997f17b7e001d50ed2d1a8d3e
ZakWegues <zakduwe@gmail.com>
2025-11-23 12:55:27 -0300

    Create Atividade1.java

---
+++ vetores/Atividade1.java
@@ -0,0 +1,45 @@
+package vetor;
+import java.util.Scanner;
+
+public class At1SeparacaoParImpar {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner (System.in);
+		
+		int vetorA[], vetorB[], indice, contadorPar=0, indiceImpar=0;
+		
+		final int TAMANHO = 20;
+		
+		vetorA = new int [TAMANHO];
+		vetorB = new int [TAMANHO];
+		
+		//Leitura de dados
+		for (indice=0; indice<TAMANHO; indice++) {
+			System.out.println("Digite o "+(indice+1)+" número");
+			//verificando números pares
+			vetorA[indice] = entrada.nextInt();
+				if (vetorA[indice]%2==0) {
+					vetorB[contadorPar] = vetorA[indice];
+					contadorPar++;
+					indiceImpar = contadorPar;
+				}
+			}
+		//verificando números impares
+		for (indice=0; indice<TAMANHO; indice++) {
+			if (vetorA[indice]%2!=0) {
+				vetorB[indiceImpar] = vetorA[indice];
+				indiceImpar++;
+			}
+		}
+		
+		System.out.print("B: [ ");
+		for (indice=0; indice<TAMANHO; indice++) {
+			System.out.print(vetorB[indice]+" ");
+		}
+		System.out.println("] ");
+		
+		entrada.close();
+
+	}
+
+}
